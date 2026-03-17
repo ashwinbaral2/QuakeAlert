bcbdd21c500986890e0020b546119b8bde533f43
ashwinbaral2 <aswinbaral2@gmail.com>
2026-03-17 12:47:18 +0545

    fix(env): configure dotenv and enable MONGO_URI from .env

--- server/database/connect.js
+++ server/database/connect.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const connectDB = async () => {
 	try {
 		const res = await mongoose.connect(
-			// process.env.MONGO_URI,
-			"mongodb://127.0.0.1:27017/earthquake_gis",
+			process.env.MONGO_URI,
+			// "mongodb://127.0.0.1:27017/earthquake_gis",
 		);
 
 		console.log("✅ MongoDB connected");
